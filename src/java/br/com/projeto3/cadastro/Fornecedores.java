4d6c714964773baf2fd2d52cb828e75154a546fb
luizhcarminati <47919205+luizhcarminati@users.noreply.github.com>
2019-03-30 20:01:15 -0300

    criação das Classes clientes e fornecedores

--- src/java/br/com/projeto3/cadastro/Fornecedores.java
+++ src/java/br/com/projeto3/cadastro/Fornecedores.java
@@ -10,5 +10,58 @@ package br.com.projeto3.cadastro;
  * @author Soryu
  */
 public class Fornecedores {
-    
+     private String nome;
+    private String rSocial;
+    private String cnpj;
+    private String email;
+    private String endereco;
+    private String telefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getrSocial() {
+        return rSocial;
+    }
+
+    public void setrSocial(String rSocial) {
+        this.rSocial = rSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
